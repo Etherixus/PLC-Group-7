fda6f2826221d37fb108e13cc46f603f0288d126
andrew <andyle295@gmail.com>
2025-10-06 10:30:28 -0400

    added comments to help reader understand code

--- projectFiles/FunctionCallNode.java
+++ projectFiles/FunctionCallNode.java
@@ -21,12 +21,20 @@ public class FunctionCallNode implements OperandNode {
         return null;
     }
 
+    //Example given the tokens from the code of ::add[5, 2, 6];
     public static FunctionCallNode parseFunctionCallNode(ArrayList<Token> tokens) throws ParserSyntaxError{
+        //check for function header (::)
         if(tokens.get(0).getTokenType() == TokenType.FC_HEADER) {
+            //check for function name (add)
             if (tokens.get(1).getTokenType() == TokenType.ID_KEYWORD) {
+                //check for l bracket ([)
                 if (tokens.get(2).getTokenType() == TokenType.L_BRACKET) {
+                    //check for parameters
                     int index = 3;
                     ArrayList<Token> params = new ArrayList<>();
+                    //until we reach a right bracket first we check if the current index is a number or an id (the bottom if statement),
+                    //next interation will check if there is a comma, if there is a comma then we check ahead to see if there is a number or id
+                    //if not then we throw and error
                     while(tokens.get(index).getTokenType() != TokenType.R_BRACKET){
                         if (tokens.get(index).getTokenType() == TokenType.COMMA){
                             if (tokens.get(index + 1).getTokenType() == TokenType.NUMBER || tokens.get(index).getTokenType() == TokenType.ID_KEYWORD){
@@ -36,17 +44,27 @@ public class FunctionCallNode implements OperandNode {
                             else {
                                 Token token = tokens.get(index);
                                 throw new ParserSyntaxError(
-                                        ParserSyntaxError.createParserSyntaxError("Expected number or ID but got " + tokens.get(0).getTokenType().toString(),
+                                        ParserSyntaxError.createParserSyntaxError("Expected number or ID but got " + tokens.get(index).getTokenType().toString(),
                                                 token.getFilename(), token.getLineNum()));
                             }
                         }
 
                         if (tokens.get(index).getTokenType() == TokenType.NUMBER || tokens.get(index).getTokenType() == TokenType.ID_KEYWORD){
                             params.add(tokens.get(index));
+                            index += 1;
+                        }
+                        else {
+                            Token token = tokens.get(index);
+                            throw new ParserSyntaxError(
+                                    ParserSyntaxError.createParserSyntaxError("Expected number or ID but got " + tokens.get(index).getTokenType().toString(),
+                                            token.getFilename(), token.getLineNum()));
                         }
                     }
+                    //check for r bracket (])
                     if (tokens.get(index + 1).getTokenType() == TokenType.R_BRACKET) {
+                        //check for r bracket (;)
                         if (tokens.get(index + 2).getTokenType() == TokenType.SEMICOLON) {
+                            //if we get a valid node then we return it and store the name and the parameters
                             return new FunctionCallNode(tokens.get(1).getToken(), params);
                         }
                     }
