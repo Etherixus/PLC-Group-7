7315ecc73c5a598328e225e3e0cc7597b460353a
Aeon999 <161261421+AoS999@users.noreply.github.com>
2025-10-15 16:39:31 -0400

    RelOpNode base case

--- projectFiles/RelOpNode.java
+++ projectFiles/RelOpNode.java
@@ -5,16 +5,37 @@ import provided.Token;
 import provided.TokenType;
 
 import java.text.ParseException;
+import java.util.ArrayList;
 
 public class RelOpNode implements JottTree {
 
-    Token relOp;
+    String relOp;
 
-
-    public RelOpNode(){
-
+    public RelOpNode(String relOp) {
+        this.relOp = relOp;
     }
 
+    public static RelOpNode parseRelOpNode(ArrayList<Token> tokenList) throws ParseException {
+        if (tokenList == null || tokenList.isEmpty()) {
+            throw new ParseException("Unexpected end of input: expected an identifier.", -1);
+        }
+
+        Token token = tokenList.get(0);
+
+        // Expecting an Relation Operator token
+        if (token.getTokenType() == TokenType.REL_OP) {
+            String keyword = token.getToken();
+            tokenList.remove(0);
+            return new RelOpNode(keyword);
+        }
+
+        // If the token isn't a Relation Operator
+        throw new ParseException(
+                "Invalid token '" + token.getToken() + "' at line " + token.getLineNum()
+                        + ": expected an identifier (variable name).",
+                token.getLineNum()
+        );
+    }
 
     @Override
     public String convertToJott() {
