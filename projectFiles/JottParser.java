954031e616c4363a1fac691e525dd59ae6ad9ae5
csd4769 <csd4769@g.rit.edu>
2025-10-18 18:40:34 -0400

    Completed Main File

--- projectFiles/JottParser.java
+++ projectFiles/JottParser.java
@@ -10,6 +10,7 @@ package projectFiles;
 import provided.JottTree;
 import provided.Token;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 
 public class JottParser {
@@ -20,8 +21,22 @@ public class JottParser {
      * @return the root of the Jott Parse Tree represented by the tokens.
      *         or null upon an error in parsing.
      */
-    public static JottTree parse(ArrayList<Token> tokens){
-		return null;
+    public static JottTree parse(ArrayList<Token> tokens) throws ParserSyntaxError, ParseException {
+        ArrayList<JottTree> nodes = new ArrayList<>();
+		if(tokens.isEmpty()) {
+            return null;
+        }
+        else{
+            while(!tokens.isEmpty()){
+                JottTree function = FunctionDefNode.parseFunctionDef(tokens);
+                if(function == null) {
+                    return null;
+                }
+                else{
+                    nodes.add(function);
+                }
+            }
+        }
+        return nodes.get(0);
     }
-    
 }
