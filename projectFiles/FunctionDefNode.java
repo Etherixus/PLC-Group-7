af651c9e5e57749649c672ef39e68719957d399a
csd4769 <csd4769@g.rit.edu>
2025-10-22 14:53:27 -0400

    FunctionDefNode.java has been adjusted to fit a new flow, FBody will be removed and logic will be put into body statement, (FBody serves no real purpose)

--- projectFiles/FunctionDefNode.java
+++ projectFiles/FunctionDefNode.java
@@ -10,10 +10,11 @@ import java.util.ArrayList;
 
 public class FunctionDefNode implements JottTree{
     private final IDNode id;
-    private final ArrayList<FunctionParamsNode> params;
+    private final FunctionParamsNode params;
     private final FunctionReturnNode returnType;
     private final FBodyNode body;
-    public FunctionDefNode(IDNode id, ArrayList<FunctionParamsNode> params, FunctionReturnNode returnType, FBodyNode body) {
+
+    public FunctionDefNode(IDNode id, FunctionParamsNode params, FunctionReturnNode returnType, FBodyNode body) {
         this.id = id;
         this.params = params;
         this.returnType = returnType;
@@ -21,80 +22,51 @@ public class FunctionDefNode implements JottTree{
     }
 
     public static FunctionDefNode parseFunctionDef(ArrayList<Token> tokens) throws ParseException, ParserSyntaxError {
-        if(tokens.isEmpty()){
-            throw new ParseException("No tokens given", -1);
+        if(!tokens.get(0).getToken().equals("Def")){
+            throw new ParserSyntaxError("Expected Def Keyword", tokens.get(0));
         }
-        else {
-            if (!tokens.get(0).getToken().equals("Def")) {
-                throw new ParseException("Function definition expected", -1);
-            }
-            else {
-                tokens.remove(0);
-                if (tokens.isEmpty() || tokens.get(0).getTokenType() != TokenType.ID_KEYWORD) {
-                    throw new ParseException("Function Def must be followed by ID", -1);
-                }
-                else {
-                    IDNode id = IDNode.parseIDNode(tokens);
-                    if(tokens.isEmpty() || tokens.get(0).getTokenType() != TokenType.L_BRACKET) {
-                        throw new ParseException("Function Def must be followed by LBracket", -1);
-                    }
-                    else{
-                        tokens.remove(0);
-                        ArrayList<FunctionParamsNode> fparams = FunctionParamsNode.parseFunctionParams(tokens);
-                        //if(tokens.isEmpty() || tokens.get(0).getTokenType() != TokenType.R_BRACKET) {
-                            //throw new ParseException("Function Params must be followed by R_BRACKET", -1);
-                        //}
-                        if(tokens.isEmpty() || tokens.get(0).getTokenType() != TokenType.COLON) {
-                            throw new ParseException("Function Params must be followed by COLON", -1);
-                        }
-                        else{
-                            tokens.remove(0);
-                            FunctionReturnNode fReturn = FunctionReturnNode.parseFunctionReturn(tokens);
-                            if(tokens.isEmpty() || tokens.get(0).getTokenType() != TokenType.L_BRACE) {
-                                throw new ParseException("Function Return must be followed by LBrace", -1);
-                            }
-                            else{
-                                tokens.remove(0);
-                                FBodyNode fBody = FBodyNode.parseFBody(tokens);
-                                if(tokens.isEmpty() || tokens.get(0).getTokenType() != TokenType.R_BRACE) {
-                                    throw new ParseException("Function Body must be followed by RBrace", -1);
-                                }
-                                else{
-                                    tokens.remove(0);
-                                    return new FunctionDefNode(id, fparams, fReturn, fBody);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+        tokens.remove(0);
+
+        IDNode funcName = IDNode.parseIDNode(tokens);
+        if(tokens.get(0).getTokenType() != TokenType.L_BRACKET){
+            throw new ParserSyntaxError("Expected Bracket Left Bracket", tokens.get(0));
         }
+        tokens.remove(0);
+        FunctionParamsNode funcParam = FunctionParamsNode.parseFunctionParams(tokens);
+        if(tokens.get(0).getTokenType() != TokenType.R_BRACKET){
+            throw new ParserSyntaxError("Expected Bracket Right Bracket", tokens.get(0));
+        }
+        tokens.remove(0);
+        FunctionReturnNode funcReturn = FunctionReturnNode.parseFunctionReturn(tokens);
+        if(tokens.get(0).getTokenType() != TokenType.COLON){
+            throw new ParserSyntaxError("Expected Colon.", tokens.get(0));
+        }
+        tokens.remove(0);
+        BodyNode body = BodyNode.parseBodyNode(tokens);
+        if(tokens.get(0).getTokenType() != TokenType.L_BRACE){
+            throw new ParserSyntaxError("Expected Bracket Left Bracket", tokens.get(0));
+        }
+        tokens.remove(0);
+        if(tokens.get(0).getTokenType() != TokenType.R_BRACE || tokens.isEmpty()){
+            throw new ParserSyntaxError("Expected RBrace Right Bracket", tokens.get(0));
+        }
+        tokens.remove(0);
+        return new FunctionDefNode(funcName, funcParam, funcReturn, body);
     }
 
     @Override
     public String convertToJott() {
-        StringBuilder sb = new StringBuilder();
+        String output = "Def";
+        output += id.convertToJott();
+        output += "[";
+        output += params.convertToJott();
+        output += "]";
+        output += returnType.convertToJott();
+        output += "{";
+        output += body.convertToJott();
+        output += "}";
+        return output;
 
-        sb.append("Def ");
-        sb.append(id.convertToJott());
-        sb.append("[");
-
-        if (params != null && !params.isEmpty()) {
-            for (int i = 0; i < params.size(); i++) {
-                sb.append(params.get(i).convertToJott());
-                if (i < params.size() - 1) {
-                    sb.append(",");
-                }
-            }
-        }
-
-        sb.append("]:");
-        sb.append(returnType.convertToJott());
-        //sb.append("{");
-        sb.append(body.convertToJott());
-        //sb.append("}");
-
-        return sb.toString();
     }
 
     @Override
