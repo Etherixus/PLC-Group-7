4c8aead78cf97e523637d31cb1262efe571d8946
csd4769 <csd4769@g.rit.edu>
2025-10-23 19:57:54 -0400

    Unused Files Moved to an unsed folder, there for possible future use

--- Unused/FBodyNode.java
+++ Unused/FBodyNode.java
@@ -1,13 +1,16 @@
-package projectFiles;
+package Unused;
 
 
+import projectFiles.BodyNode;
+import projectFiles.ParserSyntaxError;
+import projectFiles.VarDecNode;
 import provided.JottTree;
 import provided.Token;
 
 import java.text.ParseException;
 import java.util.ArrayList;
 
-public interface FBodyNode extends JottTree {
+/**public interface FBodyNode extends JottTree {
     static FBodyNode parseFBody(ArrayList<Token> tokens) throws ParseException, ParserSyntaxError {
         String currentNode = tokens.get(0).getToken();
         if(currentNode.equals("Double") || currentNode.equals("Integer") || currentNode.equals("Boolean") || currentNode.equals("String")) {
@@ -18,3 +21,4 @@ public interface FBodyNode extends JottTree {
         }
    }
 }
+ */
--- Unused/OperandNode.java
+++ Unused/OperandNode.java
@@ -1,5 +1,9 @@
-package projectFiles;
+package Unused;
 
+import projectFiles.FunctionCallNode;
+import projectFiles.IDNode;
+import projectFiles.NumberNode;
+import projectFiles.ParserSyntaxError;
 import provided.JottTree;
 import provided.Token;
 import provided.TokenType;
@@ -7,6 +11,9 @@ import provided.TokenType;
 import java.text.ParseException;
 import java.util.ArrayList;
 
+/**
+ * This code is unused when the code parses down it was getting skipped and just calling the node directly
+ *
 public interface OperandNode extends JottTree {
 
     public OperandNode parseOperand();
@@ -28,3 +35,4 @@ public interface OperandNode extends JottTree {
         }
     }
 }
+ */
