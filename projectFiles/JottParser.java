9f0f3707d0999d1ffa2f082f3aa78c6ee862ca20
csd4769 <csd4769@g.rit.edu>
2025-10-24 13:49:35 -0400

    Removed main in parser for testing, can put back later.

--- projectFiles/JottParser.java
+++ projectFiles/JottParser.java
@@ -10,7 +10,7 @@ package projectFiles;
 import provided.JottTree;
 import provided.Token;
 
-import java.text.ParseException;
+
 import java.util.ArrayList;
 
 import static projectFiles.JottTokenizer.tokenize;
@@ -19,29 +19,18 @@ public class JottParser {
 
     /**
      * Parses an ArrayList of Jotton tokens into a Jott Parse Tree.
+     *
      * @param tokens the ArrayList of Jott tokens to parse
      * @return the root of the Jott Parse Tree represented by the tokens.
-     *         or null upon an error in parsing.
+     * or null upon an error in parsing.
      */
-    public static JottTree parse(ArrayList<Token> tokens){
+    public static JottTree parse(ArrayList<Token> tokens) {
         JottTree tree;
-        try{
+        try {
             tree = ProgramNode.parseProgram(tokens);
-        }
-        catch (ParserSyntaxError syntaxError){
+        } catch (ParserSyntaxError syntaxError) {
             return null;
         }
         return tree;
-    }
-    //TODO delete before turning in
-    //expect result Def:ID_KEYWORD main:ID_KEYWORD [:L_BRACKET ]:R_BRACKET colon:COLON Void:ID_KEYWORD {:L_BRACE fcHeader:FC_HEADER print:ID_KEYWORD [:L_BRACKET 5:NUMBER ]:R_BRACKET ;:SEMICOLON fcHeader:FC_HEADER print:ID_KEYWORD [:L_BRACKET foo bar:STRING ]:R_BRACKET ;:SEMICOLON }:R_BRACE
-    public static void main(String[] args) throws ParserSyntaxError, ParseException {
-        ArrayList<Token> tokens = tokenize("provided/test.jott");
-        /**for(Token token : tokens) {
-            System.out.print(token.getToken());
-            System.out.print(token.getTokenType());
-       }*/
-        JottTree tree = parse(tokens);
-        System.out.println(tree.convertToJott());
     }
 }
