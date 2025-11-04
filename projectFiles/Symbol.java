1f7b59ecf00d574ca31fead6619f26670f78bd8e
andrew <andyle295@gmail.com>
2025-11-03 20:38:38 -0500

    implemented completed symbol and symbol table class

--- projectFiles/Symbol.java
+++ projectFiles/Symbol.java
@@ -1,9 +1,39 @@
 package projectFiles;
 
+import java.util.ArrayList;
+
+// The Symbol class represents a single named entity in a program
+// either a variable or a function.
 public class Symbol {
-    String name;
-    boolean isFunction;
+    public String name;                  // the literal name of the symbol in the source code
+    public String type;                  // data type like Integer, Double, String, Boolean, etc
+    public boolean isFunction;           // tells us if the symbol is a function or a variable
+    public ArrayList<String> paramTypes; // used for validating function calls to check if arguments match expected types
+    public String returnType;            // if function, checksif the body actaully returns the correect type
+    public boolean initialized = false;  // for tracking uninitialized variables
+    public int lineNum;                  // useful for error reporting
 
 
+    //Variable constructor
+    public Symbol(String name, String type, int lineNum) {
+        this.name = name;
+        this.type = type;
+        this.isFunction = false;
+        this.paramTypes = new ArrayList<>();
+        this.initialized = false;  // variables start uninitialized
+        this.returnType = null;    // not applicable
+        this.lineNum = lineNum;
+    }
+
+    // Function Constructor
+    public Symbol(String name, String returnType, ArrayList<String> paramTypes, int lineNum) {
+        this.name = name;
+        this.returnType = returnType;
+        this.paramTypes = paramTypes;
+        this.isFunction = true;
+        this.initialized = true;   // functions are always "defined"
+        this.type = null;          // not used for functions
+        this.lineNum = lineNum;
+    }
 
 }
--- projectFiles/SymbolTable.java
+++ projectFiles/SymbolTable.java
@@ -1,4 +1,63 @@
 package projectFiles;
 
+import java.util.HashMap;
+
 public class SymbolTable {
+    private final HashMap<String, Symbol> symbols = new HashMap<>();
+    private SymbolTable parent;
+
+    // Constructor for creating a new scope.
+    // SymbolTable global = new SymbolTable(null); (global scope)
+    // SymbolTable funcScope = new SymbolTable(global); (inside of a function)
+    public SymbolTable(SymbolTable parent){
+        this.parent = parent;
+    }
+
+    // Adds a new symbol (variable or function) to the current scope.
+    public void addSymbol(String name, Symbol symbol) throws SemanticSyntaxError {
+        if (symbols.containsKey(name)) {
+            Symbol existing = symbols.get(name);
+            throw new SemanticSyntaxError("Redeclaration of symbol" + name +
+                    "' (previously declare on line " + existing.lineNum + ")");
+        }
+        symbols.put(name, symbol);
+    }
+
+    // Looks up a symbol by name in this scope or its parent scopes.
+    // Lookup Order
+    // 1. Current Scope (for local variables)
+    // 2. Parent Scope (for variables in parent scope or global func/vars
+    // returns the symbol if found or null if not declared
+    public Symbol lookup(String name) {
+        Symbol symbol = symbols.get(name);
+        if (symbol != null) return symbol;
+        if (parent != null) return parent.lookup(name);
+        return null;
+    }
+
+
+     // Marks a variable as initialized after assignment.
+     // Safe no-op for functions (ignored if isFunction == true).
+    public void markInitialized(String name) {
+        Symbol s = lookup(name);
+        if (s != null && !s.isFunction) {
+            s.initialized = true;
+        }
+    }
+
+    //Checks whether a variable was initialized before being used.
+    //Throws a SemanticError if used before initialization.
+    public void checkInitialized(String name, int currentLine) throws SemanticSyntaxError {
+        Symbol s = lookup(name);
+        if (s != null) {
+            throw new SemanticSyntaxError("Undeclared variable: '" + name + "'");
+        }
+
+        if (!s.isFunction && !s.initialized){
+            throw new SemanticSyntaxError(
+                    "Variable '" + name + "' used before initialization " +
+                            "(declared on line " + s.lineNum + ")"
+            );
+        }
+    }
 }
